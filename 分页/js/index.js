7133da73d309cfc04ccfe267df57be7b735c21e0
Andyjhl <460200123@qq.com>
2017-08-08 16:03:00 +0800

    paging7

--- 分页/js/index.js
+++ 分页/js/index.js
@@ -7,14 +7,13 @@ var pageRender = (function () {
         numBox = document.getElementById('numBox'),
         pageNum = document.getElementById('pageNum');
 
-    //->SEND AJAX AND BIND HTML
+    //->实现页面的数据绑定
     function bindHTML() {
-        //->SUCCESS AND BIND HTML
+        //->回调函数
         function callback(result) {
             if (!result) return;
-            total = parseInt(result['total']);
+            total = parseInt(result['total']);// 获取页面的总页数
 
-            //->GET DATA CONCAT STR
             var data = result['data'],
                 str = '';
             for (var i = 0, len = data.length; i < len; i++) {
@@ -27,8 +26,7 @@ var pageRender = (function () {
                 str += '</li>';
             }
             content.innerHTML = str;
-
-            //->CONCAT PAGE
+            //->设置页面的总页数
             str = '';
             for (i = 1; i <= total; i++) {
                 if (i == n) {
@@ -38,22 +36,21 @@ var pageRender = (function () {
                 str += '<li>' + i + '</li>';
             }
             numBox.innerHTML = str;
-
-            //->PAGE NUM SHOW N
+            //->设置输入框中的值，就是当前页的值
             pageNum.value = n;
         }
 
-        //->SEND AJAX GET DATA
+        //->用ajax发送请求获取数据
         ajax({
             url: '/getList?n=' + n,
             type: 'GET',
             dataType: "JSON",
             cache: false,
-            success: callback
+            success: callback //成功后执行回调函数
         });
     }
 
-    //->BIND EVENT
+    //->绑定事件
     function bindEvent() {
         oBox.onclick = function (ev) {
             ev = ev || window.event;
@@ -62,7 +59,6 @@ var pageRender = (function () {
                 tarInn = tar.innerHTML,
                 tarParent = tar.parentNode,
                 tarGrandparent = tarParent.parentNode;
-
             //->CONTENT LI OR SPAN
             if ((tarTag === 'LI' && tarParent.id === 'content') || (tarTag === 'SPAN' && tarGrandparent.id === 'content')) {
                 if (tarTag === 'SPAN') {
@@ -73,7 +69,6 @@ var pageRender = (function () {
                 window.open('detail.html?id=' + id);
                 return;
             }
-
             //->NUM BOX LI
             if (tarTag === 'LI' && tarParent.id === 'numBox') {
                 tarInn = parseInt(tarInn);
@@ -113,23 +108,20 @@ var pageRender = (function () {
                 }
                 bindHTML();
             }
-
         }
     }
-
     return {
+        //模块的入口，初始化设置
         init: function () {
             //->BIND HTML
             bindHTML();
-
             //->BIND EVENT
             bindEvent();
-
             //->INPUT KEY UP
             pageNum.onkeyup = function (ev) {
                 ev = ev || window.event;
                 //->ENTER
-                if (ev.keyCode === 13) {
+                if (ev.keyCode === 13) { //ev.keyCode == 13：代表的是回车键
                     var val = parseFloat(this.value);
                     if (isNaN(val) || val === n) {
                         this.value = n;
