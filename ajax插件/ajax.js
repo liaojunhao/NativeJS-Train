31458d466f8eaf7d59e9bc3cddb116568b0ac557
Andyjhl <460200123@qq.com>
2017-08-10 14:06:13 +0800

    日历插件

---
+++ ajax插件/ajax.js
@@ -0,0 +1,8 @@
+/*
+*
+* @Name：ljhAjax v1.1 ajax插件
+* @Author：andyjhl
+* @Date：2017-02-10
+*
+* */
+
---
+++ 日历插件/LjhDate.js
@@ -0,0 +1,28 @@
+/*
+*
+* @Name：LjhDate v1.1 日期控件
+* @Author：andyjhl
+* @Date：2017-05-10
+*
+* */
+
+(function(win){
+
+    var config =  {
+        path: '', //laydate所在路径
+        skin: 'default', //初始化皮肤
+        format: 'YYYY-MM-DD', //日期格式
+        min: '1900-01-01 00:00:00', //最小日期
+        max: '2099-12-31 23:59:59', //最大日期
+        isv: false,
+        init: true
+    };
+
+    var Dates = {},
+        doc = document,
+        creat = 'createElement',
+        byid = 'getElementById',
+        tags = 'getElementsByTagName';
+
+
+})(window)
