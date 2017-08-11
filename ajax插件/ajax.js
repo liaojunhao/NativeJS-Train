ef8d180b383e8fd86940a2f18823376f78b8670f
Andyjhl <460200123@qq.com>
2017-08-11 14:44:32 +0800

    ajax插件

--- ajax插件/ajax.js
+++ ajax插件/ajax.js
@@ -6,3 +6,68 @@
 *
 * */
 
+function createXHR() {
+    var xhr = null;
+    var flag = false;
+    var ary = [
+        function () {
+            return new XMLHttpRequest();
+        },
+        function () {
+            return new ActiveXObject("Microsoft.XMLHTTP");
+        },
+        function () {
+            return new ActiveXObject("Msxml2.XMLHTTP")
+        },
+        function () {
+            return new ActiveXObject("Msxml3.XMLHTTP")
+        }
+    ];
+    for(var i=0;i<ary.length;i++){
+        var curFn = ary[i];
+        try{
+            xhr = curFn();
+            createXHR = curFn;
+            flag =true;
+            return xhr;
+        }catch(e){
+
+        }
+    }
+    if(!flag){
+        throw new Error("你的浏览器不支持ajax");
+    }
+}
+
+function ajax(options) {
+    // 初始化常用的参数
+    var _default = {
+        url : "",
+        type : "get",
+        dataType : "json",
+        async : true,
+        data : null,
+        timeout : 1000,//请求超时
+        success : null
+    }
+    for(var key in options){
+        if(options.hasOwnProperty(key)){
+            _default[key] = options[key];
+        }
+    }
+
+    var xhr = createXHR();// 创建的ajax 的对象
+
+    xhr.open(_default.type,_default.url,_default.async);
+
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState === 4 && /^2\d{2}/.test(xhr.status)){
+            var val = xhr.responseText;
+            if(_default.dataType === "json"){
+                val = "JSON" in window?JSON.parse(val):eval("("+val+")");
+            }
+            _default.success.call(xhr,val)
+        }
+    }
+    xhr.send(_default.data);
+}
