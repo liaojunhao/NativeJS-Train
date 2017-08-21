3b6642cc125f33287840836d3018b748b28c1569
liaojunhao <460200123@qq.com>
2017-08-21 17:50:08 +0800

    正则应用3

--- 正则应用/时间字符串格式化.js
+++ 正则应用/时间字符串格式化.js
@@ -4,6 +4,7 @@
 *
 * */
 
+//方法一
 String.prototype.myFormatTime = function myFormatTime() {
     var reg = /^(\d{4})(?:-|\/|\.|:)(\d{1,2})(?:-|\/|\.|:)(\d{1,2})(?: +)?(\d{1,2})?(?:-|\/|\.|:)?(\d{1,2})?(?:-|\/|\.|:)?(\d{1,2})?$/g,
         ary = [];
@@ -21,3 +22,18 @@ var str = "2016-5-23 18:24:6";
 console.log(str.myFormatTime("{0}年{1}月{2}日"));
 console.log(str.myFormatTime("{1}-{2}"));
 console.log(str.myFormatTime("{0}年{1}月{2}日 {3}:{4}:{5}"));
+
+
+//方法二：少写点正则
+String.prototype.myFormatTime = function(template) {
+    var res = null,
+        ary = this.match(/\d+/g);
+    template = template || "{0}年{1}月{2}日 {3}时{4}分{5}秒";
+    res = template.replace(/\{(\d+)\}/g, function () {
+        var val = ary[arguments[1]];
+        !val ? val = "00" : null;
+        val.length < 2 ? val = "0" + val : null;
+        return val;
+    });
+    return res;
+}
