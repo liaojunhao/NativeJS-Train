d945f202bc341d04ab2cd6a200e65cd9cbe7d624
liaojunhao <460200123@qq.com>
2017-08-27 17:10:00 +0800

    数组排序2

---
+++ 数组排序/数组乱序排序.js
@@ -0,0 +1,14 @@
+//数组乱序排序
+var arr = [1,2,3,4,5,6,7,8]
+
+function shuffle(a) {
+    var b = [];
+    while (a.length > 0) {
+        var index = parseInt(Math.random() * (a.length - 1));
+        b.push(a[index]);
+        a.splice(index, 1);//把原数组对应索引上的那个值删掉
+    }
+    return b;
+}
+
+console.log(shuffle(arr))
