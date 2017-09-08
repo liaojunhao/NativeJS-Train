1a2aeac3ceb8b97de335f7b3efa0f608ce4d8530
liaojunhao <460200123@qq.com>
2017-09-08 16:08:09 +0800

    cookie2

--- cookie/cookie.js
+++ cookie/cookie.js
@@ -1,20 +1,46 @@
 var cookie = {
-    set: function (name, value, expires, path, domain) {
-        if (typeof expires === "undefined") {
-            expires = new Date(new Date().getTime() + (1000 * 60 * 60 * 24));
+    add: function(key, val, h) {
+        var str = key + "=" + escape(val);
+        // 如果h为0的时候，就是不设置过期时间，浏览器关闭的时候cookie自动删除
+        if (h > 0) {
+            var date = new Date();
+            var ms = h * 3600 * 1000;
+            date.setTime(date.getTime() + ms);
+            str += "; expires=" + date.toGMTString();//根据格林威治时间(GTM)把Date对象转换为字符串
         }
-        document.cookie = name + "=" + escape(value) + ((expires) ? ";expires=" + expires.toGMTString() : "") + ((path) ? ";path=" + path : ";path=/") + ((domain) ? ";domain=" + domain : "");
+        document.cookie = str;
     },
-    get: function (name) {
-        var arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
-        if (arr != null) {
-            return unescape(arr[2]);
+    get: function(key) {
+        var arrStr = document.cookie.split("; ");
+        for (var i = 0; i < arrStr.length; i++) {
+            var temp = arrStr[i].split("=");
+            if (temp[0] == key) {
+                return unescape(temp[1]);
+            }
         }
-        return null;
     },
-    clear: function (name, path, domain) {
-        if (this.get(name)) {
-            document.cookie = name + "=" + ((path) ? "; path=" + path : "; path=/") + ((domain) ? "; domain=" + domain : "") + ";expires=Fri, 02-Jan-1970 00:00:00 GMT";
+    del: function(key) {
+        var date = new Date();
+        date.setTime(-10000);
+        document.cookie = key + "=a; expires=" + date.toGMTString();
+    },
+    getAll: function() {
+        var str = document.cookie;
+        var arr = document.cookie.split("; ");
+        var valArr = [];
+        if (!str) {
+            console.log("您没有保存任何Cookie");
+        }
+        else
+        {
+
+            for(var i = 0; i < arr.length; i++) {
+                var temp = arr[i].split("=");
+                valArr.push("Key:" + temp[1] + ", Val:" + unescape(temp[1]));
+            }
+            return valArr;
         }
     }
 };
+
+export default cookie
